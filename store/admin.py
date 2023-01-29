d1d79dd9b32ef372dd4884c5c2435f3ba21575c9
mhddanny <mhddanny1gmail.com>
2023-01-29 21:30:52 +0700

    this is my first commit

---
+++ store/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from . models import Product
+
+# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    '''Admin View for Product'''
+
+    list_display = ('product_name', 'price', 'stock', 'category', 'modifield_date', 'is_available')
+    prepopulated_fields = {'slug': ('product_name',)}
+    # list_filter = ('',)
+    # raw_id_fields = ('',)
+    # readonly_fields = ('',)
+    # search_fields = ('',)
+    # date_hierarchy = ''
+    # ordering = ('',)
