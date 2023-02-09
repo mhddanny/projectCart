78fa0b955d89d0c351e0f4150d868fec173de3ac
mhddanny <mhddanny1gmail.com>
2023-02-09 22:21:06 +0700

    Product Order and Payments

--- projectCart/settings.py
+++ projectCart/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 
 USE_I18N = True
 
