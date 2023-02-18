d5f84dc98d2d52a7343c428f4d29d4ff2959eb98
mhddanny <mhddanny1gmail.com>
2023-02-18 20:51:46 +0700

    Updata Host

--- projectCart/settings.py
+++ projectCart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) #True
 
-ALLOWED_HOSTS = ["projectCart-env-dev.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ['projectCart-env.eba-zrqszuft.us-west-2.elasticbeanstalk.com']
 
 # Application definition
 
