b75d15b530b42605e9f23839a44c48881641cc24
mhddanny <mhddanny1gmail.com>
2023-02-19 11:54:58 +0700

    Update Allow Host

--- projectCart/settings.py
+++ projectCart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) #True
 
-ALLOWED_HOSTS = ['projectCar-test-env.eba-b98k8pzd.us-west-2.elasticbeanstalk.com','127.0.0.1']
+ALLOWED_HOSTS = ['projectCar-test-env.eba-b98k8pzd.us-west-2.elasticbeanstalk.com','*']
 
 # Application definition
 
