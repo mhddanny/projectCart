c6adea8525b83d8494ad76c6cbd67a47ccb6d7ff
mhddanny <mhddanny1gmail.com>
2023-02-19 21:55:40 +0700

    update query product

--- projectCart/views.py
+++ projectCart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from store.models import Product, ReviewRating
 
 def home (request):
-    products = Product.objects.all().filter(is_available=True).order_by('-created_date')
+    products = Product.objects.all().filter(is_available=True).order_by('created_date')
 
     # Get The reviews
     reviews = None
