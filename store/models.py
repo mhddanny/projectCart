28688aa23072e0f04234ea7da3d457e612b880f9
mhddanny <mhddanny1gmail.com>
2023-02-22 10:04:10 +0700

    update store produt model

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     modifield_date = models.DateTimeField(auto_now=True)
 
     def get_url(self):
